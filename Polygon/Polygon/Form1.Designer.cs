6abe2a38b05d94a8759c0bcbee7956ef4efcc335
DIsakov <55271574+DIsakov14504@users.noreply.github.com>
2021-03-31 19:05:49 +0300

    Минизадача 8 вроде

---
+++ Polygon/Polygon/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Polygon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.nodeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.triangleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.circleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.squareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nodeColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.speedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.radiusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nodeToolStripMenuItem,
+            this.nodeColorToolStripMenuItem,
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem,
+            this.speedToolStripMenuItem,
+            this.radiusToolStripMenuItem,
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1019, 40);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // nodeToolStripMenuItem
+            // 
+            this.nodeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.triangleToolStripMenuItem,
+            this.circleToolStripMenuItem,
+            this.squareToolStripMenuItem});
+            this.nodeToolStripMenuItem.Name = "nodeToolStripMenuItem";
+            this.nodeToolStripMenuItem.Size = new System.Drawing.Size(94, 36);
+            this.nodeToolStripMenuItem.Text = "Node";
+            // 
+            // triangleToolStripMenuItem
+            // 
+            this.triangleToolStripMenuItem.Name = "triangleToolStripMenuItem";
+            this.triangleToolStripMenuItem.Size = new System.Drawing.Size(233, 44);
+            this.triangleToolStripMenuItem.Text = "Triangle";
+            this.triangleToolStripMenuItem.Click += new System.EventHandler(this.triangleToolStripMenuItem_Click);
+            // 
+            // circleToolStripMenuItem
+            // 
+            this.circleToolStripMenuItem.Name = "circleToolStripMenuItem";
+            this.circleToolStripMenuItem.Size = new System.Drawing.Size(233, 44);
+            this.circleToolStripMenuItem.Text = "Circle";
+            this.circleToolStripMenuItem.Click += new System.EventHandler(this.circleToolStripMenuItem_Click);
+            // 
+            // squareToolStripMenuItem
+            // 
+            this.squareToolStripMenuItem.Name = "squareToolStripMenuItem";
+            this.squareToolStripMenuItem.Size = new System.Drawing.Size(233, 44);
+            this.squareToolStripMenuItem.Text = "Square";
+            this.squareToolStripMenuItem.Click += new System.EventHandler(this.squareToolStripMenuItem_Click);
+            // 
+            // nodeColorToolStripMenuItem
+            // 
+            this.nodeColorToolStripMenuItem.Name = "nodeColorToolStripMenuItem";
+            this.nodeColorToolStripMenuItem.Size = new System.Drawing.Size(151, 36);
+            this.nodeColorToolStripMenuItem.Text = "NodeColor";
+            this.nodeColorToolStripMenuItem.Click += new System.EventHandler(this.nodeColorToolStripMenuItem_Click);
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.BackColor = System.Drawing.Color.Lime;
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(83, 36);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.BackColor = System.Drawing.Color.Red;
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(83, 36);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // speedToolStripMenuItem
+            // 
+            this.speedToolStripMenuItem.Name = "speedToolStripMenuItem";
+            this.speedToolStripMenuItem.Size = new System.Drawing.Size(102, 36);
+            this.speedToolStripMenuItem.Text = "Speed";
+            this.speedToolStripMenuItem.Click += new System.EventHandler(this.speedToolStripMenuItem_Click);
+            // 
+            // radiusToolStripMenuItem
+            // 
+            this.radiusToolStripMenuItem.Name = "radiusToolStripMenuItem";
+            this.radiusToolStripMenuItem.Size = new System.Drawing.Size(105, 36);
+            this.radiusToolStripMenuItem.Text = "Radius";
+            this.radiusToolStripMenuItem.Click += new System.EventHandler(this.radiusToolStripMenuItem_Click);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.newToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(72, 36);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1019, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "  ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem nodeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem triangleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem circleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem squareToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nodeColorToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem speedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem radiusToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+    }
+}
+
---
+++ Polygon/Polygon/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Polygon
+{
+    public partial class Form1 : Form
+    {
+        List<Node> nodes = new();
+        int nShape;
+        bool ifDragNode;
+        static Random random;
+        int Radius;
+        Form3 boo;
+        string path;
+        public Form1()
+        {
+            InitializeComponent();
+            nShape = 0;
+            random = new Random();
+            ifDragNode = false;
+            Radius = Node.r;
+        }
+        private void Form1_Load(object sender, EventArgs e) { DoubleBuffered = true; }
+        private void triangleToolStripMenuItem_Click(object sender, EventArgs e) { nShape = 0; }
+        private void circleToolStripMenuItem_Click(object sender, EventArgs e) { nShape = 1; }
+        private void squareToolStripMenuItem_Click(object sender, EventArgs e) { nShape = 2; }
+        private void startToolStripMenuItem_Click(object sender, EventArgs e){ timer1.Start(); }
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e){ timer1.Stop(); }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Node node in nodes) node.DrawNode(e.Graphics);
+            if (nodes.Count > 2)
+            {
+                foreach (Node node1 in nodes)
+                {
+                    node1.anyLine = false;
+                    foreach (Node node2 in nodes)
+                    {
+                        node2.anyLine = false;
+                        if (node1 == node2) continue;
+                        int up = 0;
+                        int down = 0;
+                        foreach (Node node3 in nodes)
+                        {
+                            if (node3 == node1 || node3 == node2) continue;
+                            if ((node3.SetY - node1.SetY) * (node2.SetX - node1.SetX) > (node3.SetX - node1.SetX) * (node2.SetY - node1.SetY)) up++;
+                            else down++;
+                        }
+                        if (up == 0 || down == 0)
+                        {
+                            e.Graphics.DrawLine(new Pen(Color.Red), node1.SetX, node1.SetY, node2.SetX, node2.SetY);
+                            node1.anyLine = true;
+                            node2.anyLine = true;
+                        }
+                    }
+                }
+            }
+        }
+        private bool IsInsidePolygon(int x, int y)
+        {
+            Triangle node0 = new(x, y);
+            int up;
+            int down;
+            node0.anyLine = false;
+            foreach (Node node1 in nodes)
+            {
+                if (node0.SetX == node1.SetX && node0.SetY == node1.SetY) continue;
+                up = 0;
+                down = 0;
+                foreach (Node node2 in nodes)
+                {
+                    if (node1 == node2) continue;
+                    else if (node0.SetX == node2.SetX && node0.SetY == node2.SetY) continue;
+                    if ((node2.SetY - node0.SetY) * (node1.SetX - node0.SetX) > (node2.SetX - node0.SetX) * (node1.SetY - node0.SetY)) up++;
+                    else down++;
+                }
+                if (up == 0 || down == 0)
+                {
+                    node0.anyLine = true;
+                    break;
+                }
+            }
+            if (!node0.anyLine) return true;
+            return false;
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (nodes.Any())
+            {
+                ifDragNode = false;
+                foreach (Node node in nodes) 
+                {
+                    if (node.Check(e.X, e.Y))
+                    {
+                        ifDragNode = true;
+                        if (e.Button == MouseButtons.Left)
+                        {
+                            node.drag = true;
+                            node.dx = e.X - node.SetX;
+                            node.dy = e.Y - node.SetY;
+                        }
+                        else
+                        {
+                            nodes.Remove(node);
+                            Refresh();
+                            break;
+                        }
+                    }
+                }
+                if (IsInsidePolygon(e.X, e.Y) && !ifDragNode && nodes.Count > 2) 
+                {
+                    foreach (Node node in nodes)
+                    {
+                        node.drag = true;
+                        node.dx = e.X - node.SetX;
+                        node.dy = e.Y - node.SetY;
+                    }
+                }
+                else if (!IsInsidePolygon(e.X, e.Y) && !ifDragNode){ 
+                    switch (nShape)
+                    {
+                        case 0:
+                            nodes.Add(new Triangle(e.X, e.Y));
+                            break;
+                        case 1:
+                            nodes.Add(new Circle(e.X, e.Y));
+                            break;
+                        case 2:
+                            nodes.Add(new Square(e.X, e.Y));
+                            break;
+                    }
+                    Refresh();
+                }
+            }
+            else
+            {
+                switch (nShape)
+                {
+                    case 0:
+                        nodes.Add(new Triangle(e.X, e.Y));
+                        break;
+                    case 1:
+                        nodes.Add(new Circle(e.X, e.Y));
+                        break;
+                    case 2:
+                        nodes.Add(new Square(e.X, e.Y));
+                        break;
+                }
+                Refresh();
+            }
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            foreach (Node node in nodes)
+            {
+                if (node.drag)
+                {
+                    node.SetX = e.X - node.dx;
+                    node.SetY = e.Y - node.dy;
+                    Refresh();
+                }
+            }
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            foreach (Node node in nodes) node.drag = false;
+            if (nodes.Count > 2)
+            {
+                int i = 0;
+                int k = nodes.Count;
+                for (; i < k ; i++)
+                {
+                    if (IsInsidePolygon(nodes[i].SetX, nodes[i].SetY))
+                    {
+                        nodes.Remove(nodes[i]);
+                        i--;
+                        k--;
+                    }
+                }
+            }
+            Refresh();
+        }
+        private void nodeColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new();
+            colorDialog.AllowFullOpen = false;
+            colorDialog.Color = Node.color;
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                Node.color = colorDialog.Color;
+                Refresh();
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int k;
+            foreach (Node node in nodes)
+            {
+                k = random.Next(-5, 5);
+                node.SetX += k;
+                k = random.Next(-5, 5);
+                node.SetY += k;
+            }
+            if (nodes.Count > 2)
+            {
+                for (int i = 0; i < nodes.Count; i++)
+                {
+                    if (IsInsidePolygon(nodes[i].SetX, nodes[i].SetY))
+                    {
+                        nodes.Remove(nodes[i]);
+                    }
+                }
+            }
+            Refresh();
+        }
+        private void speedToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 form = new(timer1.Interval);
+            form.ShowDialog();
+            timer1.Interval = form.t;
+        }
+        private void RadiusDelegate(object sender, RadiusEventArgs e)
+        {
+            Radius = e.R;
+            Node.r = Radius;
+            Refresh();
+        }
+        private void radiusToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (boo == null || boo.IsDisposed)
+            {
+                boo = new Form3(Radius);
+                boo.Show();
+                boo.RCh += RadiusDelegate;
+            }
+            else
+            {
+                boo.Show();
+                boo.RCh += RadiusDelegate;
+            }
+            boo.Focus();
+        }
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new() { InitialDirectory = @"..\..\Images", Filter = "Binary files(*.dat) | *.dat" };
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                Stream stream = open.OpenFile();
+                BinaryFormatter bf = new();
+                List<object> info = (List<object>)bf.Deserialize(stream);
+                nodes.Clear();
+                if (info.Count > 2) for (int i = 0; i < info.Count - 2; i++) nodes.Add((Node)info[i]);
+                Node.color = (Color)info[info.Count - 2];
+                Node.r = (int)info[info.Count - 1];
+                stream.Close();
+                Refresh();
+            }
+        }
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new() { InitialDirectory = @"..\..\Images", Filter = "Binary files(*.dat) | *.dat" };
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                Stream stream = save.OpenFile();
+                BinaryFormatter bf = new();
+                List<object> info = new();
+                foreach (Node node in nodes) info.Add(node);
+                info.Add(Node.color);
+                info.Add(Node.r);
+                bf.Serialize(stream, info);
+                stream.Close();
+                Refresh();
+            }
+        }
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            nodes.Clear();
+            Node.color = Color.Turquoise;
+            Node.r = 50;
+            timer1.Stop();
+            timer1.Interval = 100;
+            nShape = 0;
+            Refresh();
+        }
+    }
+}
